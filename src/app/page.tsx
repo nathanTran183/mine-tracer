a5c9d0de9c5b79d5f3a46b6bf5015c07448a1b95
nathantran183 <phuctd1803@gmail.com>
2025-07-09 23:49:30 +0800

    Fix merge conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="grid items-center justify-items-center  gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center justify-items-center gap-16 font-[family-name:var(--font-geist-sans)]">
       <div>{children}</div>
     </div>
   );
